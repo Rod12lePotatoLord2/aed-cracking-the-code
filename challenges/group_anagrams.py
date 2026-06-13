c42d50e6f7075088fda8e6f014f0553d0d4285fc
Rodrigo Lima <rodrigolimabarbosa.12@gmail.com>
2026-06-13 14:15:14 -0300

    style: remove espaços em branco das linhas vazias para passar no linter

--- challenges/group_anagrams.py
+++ challenges/group_anagrams.py
@@ -1,12 +1,12 @@
 def group_anagrams(words: list[str]) -> list[list[str]]:
     anagramas = {}
-    
+
     for palavra in words:
         chave = "".join(sorted(palavra))
-        
+
         if chave not in anagramas:
             anagramas[chave] = []
-            
+
         anagramas[chave].append(palavra)
-        
+
     return list(anagramas.values())
