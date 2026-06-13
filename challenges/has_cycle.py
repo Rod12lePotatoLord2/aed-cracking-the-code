ec7735dd4e7d7869a438039b72d929556e0be696
Rodrigo Lima <rodrigolimabarbosa.12@gmail.com>
2026-06-13 13:59:03 -0300

    feat: implementa deteccao de ciclos em grafos usando DFS com O(V + E)

--- challenges/has_cycle.py
+++ challenges/has_cycle.py
@@ -1,2 +1,26 @@
-def has_cycle(graph: dict[str, list[str]]) -> bool:
-    raise NotImplementedError
+def has_cycle(graph: dict) -> bool:
+    visitados = set()
+    pilha_recursao = set()
+
+    def dfs(node) -> bool:
+        if node in pilha_recursao:
+            return True
+        if node in visitados:
+            return False
+
+        pilha_recursao.add(node)
+        visitados.add(node)
+
+        for vizinho in graph.get(node, []):
+            if dfs(vizinho):
+                return True
+
+        pilha_recursao.remove(node)
+        return False
+
+    for vertex in graph:
+        if vertex not in visitados:
+            if dfs(vertex):
+                return True
+
+    return False
