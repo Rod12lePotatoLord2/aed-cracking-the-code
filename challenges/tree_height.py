7a757f63a3085d8b85a8d6613fcbedfd0669685f
Rodrigo Lima <rodrigolimabarbosa.12@gmail.com>
2026-06-13 14:03:16 -0300

    feat: implementa calculo da altura de uma arvore binaria com O(n)

--- challenges/tree_height.py
+++ challenges/tree_height.py
@@ -2,4 +2,10 @@ from data_structures.node import Node
 
 
 def tree_height(root: Node | None) -> int:
-    raise NotImplementedError
+    if root is None:
+        return -1
+
+    altura_esquerda = tree_height(root.left)
+    altura_direita = tree_height(root.right)
+
+    return 1 + max(altura_esquerda, altura_direita)
